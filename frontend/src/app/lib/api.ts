b560b3d7a01837860d4d9bce3c1e6e8316147107
root <root@hk.hk>
2025-10-29 21:47:09 -0300

    Update

--- frontend/src/app/lib/api.ts
+++ frontend/src/app/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://www.urlcurt.site/api';
 
 export const api = axios.create({
   baseURL: BASE_URL,
