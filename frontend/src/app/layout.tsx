e0a0fb33865ab219ea29201873bbfa9001d1753d
root <root@hk.hk>
2025-12-25 10:29:23 -0300

    O README FOI ATUALIZADO

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script"; // 👈 IMPORT NECESSÁRIO
 
 export const metadata: Metadata = {
   title: "UrlCurt",
@@ -17,6 +18,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6BV36MJBP2"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6BV36MJBP2');
+          `}
+        </Script>
+      </head>
       <body className="bg-gray-50 antialiased" suppressHydrationWarning>
         {children}
       </body>
