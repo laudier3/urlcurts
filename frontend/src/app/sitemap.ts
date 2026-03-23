acdde754491600dd29cc26d445de93ae0ded4dbb
root <root@hk.hk>
2026-03-23 19:48:59 -0300

    primeiro commit

---
+++ frontend/src/app/sitemap.ts
@@ -0,0 +1,12 @@
+// app/sitemap.ts
+
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.urlcurt.com.br",
+    },
+    {
+      url: "https://www.urlcurt.com.br/blog/melhor-encurtador-de-link",
+    },
+  ];
+}
